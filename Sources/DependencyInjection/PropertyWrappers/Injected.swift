16600908e2e02bc47d4431d68e8337181c9e5082
Tyler Thompson <tyler.keith.thompson@gmail.com>
2024-08-05 14:41:14 -0600

    [main] - Injected tested - TT
    Co-authored-by: Tyler Thompson <tyler.thompson@wwt.com>

--- Sources/DependencyInjection/PropertyWrappers/Injected.swift
+++ Sources/DependencyInjection/PropertyWrappers/Injected.swift
@@ -6,17 +6,34 @@
 //
 
 @propertyWrapper
-public struct Injected<Value>: Sendable {
-    let factory: SyncFactory<Value>
-    public init(_ factory: SyncFactory<Value>) {
+public struct Injected<Value, Factory: Sendable>: Sendable {
+    let factory: Factory
+    let getter: @Sendable () -> Value
+    public init(_ factory: Factory) where Factory == SyncFactory<Value> {
+        getter = { factory() }
+        self.factory = factory
+    }
+    
+    public init<D>(_ factory: Factory) where Factory == SyncThrowingFactory<D>, Value == Result<D, any Error> {
+        getter = { Result { try factory() } }
+        self.factory = factory
+    }
+    
+    public init<D>(_ factory: Factory) where Factory == AsyncFactory<D>, Value == Task<D, Never> {
+        getter = { Task { await factory() } }
+        self.factory = factory
+    }
+    
+    public init<D>(_ factory: Factory) where Factory == AsyncThrowingFactory<D>, Value == Task<D, any Error> {
+        getter = { Task { try await factory() } }
         self.factory = factory
     }
     
     public var wrappedValue: Value {
-        factory()
+        getter()
     }
     
-    public var projectedValue: SyncFactory<Value> {
+    public var projectedValue: Factory {
         factory
     }
 }
---
+++ Tests/DependencyInjectionTests/InjectedPropertyWrapperTests.swift
@@ -0,0 +1,115 @@
+//
+//  InjectedPropertyWrapperTests.swift
+//  DependencyInjection
+//
+//  Created by Tyler Thompson on 8/5/24.
+//
+
+import Testing
+import DependencyInjection
+
+struct InjectedPropertyWrapperTests {
+    @Test func injectedPropertyWrapper_WithSyncFactory_ResolvesEveryTime() async throws {
+        class Example {
+            @Injected(Container.exampleDependency) var dependency
+        }
+        
+        let expected = ExampleDependency()
+        var count = 0
+        Container.exampleDependency.register {
+            count += 1
+            return expected
+        }
+        let example = Example()
+        #expect(example.dependency === expected)
+        #expect(example.dependency === expected)
+        #expect(count == 2)
+    }
+    
+    @Test func injectedPropertyWrapper_WithSyncThrowingFactory_ResolvesEveryTime() async throws {
+        class Example {
+            @Injected(Container.exampleThrowingDependency) var dependency
+        }
+        
+        let expectedResult = Result { try ExampleThrowingDependency() }
+        let expected = try expectedResult.get()
+        var count = 0
+        Container.exampleThrowingDependency.register {
+            count += 1
+            return try expectedResult.get()
+        }
+        let example = Example()
+        #expect(try example.dependency.get() === expected)
+        #expect(try example.dependency.get() === expected)
+        #expect(count == 2)
+    }
+    
+    @Test func injectedPropertyWrapper_WithAsyncFactory_ResolvesEveryTime() async throws {
+        class Example {
+            @Injected(Container.exampleAsyncDependency) var dependency
+        }
+        
+        let expectedResult = Task { await ExampleAsyncDependency() }
+        let expected = await expectedResult.value
+        actor Test {
+            var count = 0
+            func increment() {
+                count += 1
+            }
+        }
+        let test = Test()
+        Container.exampleAsyncDependency.register {
+            await test.increment()
+            return await expectedResult.value
+        }
+        let example = Example()
+        #expect(await example.dependency.value === expected)
+        #expect(await example.dependency.value === expected)
+        #expect(await test.count == 2)
+    }
+    
+    @Test func injectedPropertyWrapper_WithAsyncThrowingFactory_ResolvesEveryTime() async throws {
+        class Example {
+            @Injected(Container.exampleAsyncThrowingDependency) var dependency
+        }
+        
+        let expectedResult = Task { try await ExampleAsyncThrowingDependency() }
+        let expected = try await expectedResult.value
+        actor Test {
+            var count = 0
+            func increment() {
+                count += 1
+            }
+        }
+        let test = Test()
+        Container.exampleAsyncThrowingDependency.register {
+            await test.increment()
+            return try await expectedResult.value
+        }
+        let example = Example()
+        #expect(try await example.dependency.value === expected)
+        #expect(try await example.dependency.value === expected)
+        #expect(try await test.count == 2)
+    }
+}
+
+class ExampleDependency { }
+
+class ExampleThrowingDependency {
+    init() throws { }
+}
+
+class ExampleAsyncDependency {
+    init() async { }
+}
+
+class ExampleAsyncThrowingDependency {
+    init() async throws { }
+}
+
+extension Container {
+    static let exampleDependency = Factory { ExampleDependency() }
+    static let exampleThrowingDependency = Factory { try ExampleThrowingDependency() }
+    static let exampleAsyncDependency = Factory { await ExampleAsyncDependency() }
+    static let exampleAsyncThrowingDependency = Factory { try await ExampleAsyncThrowingDependency() }
+}
